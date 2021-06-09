31805da3ab7265d751046abc7f09bf2a1297d7f1
Mehmet Efe <mehmetefeyuzuguler@gmail.com>
2021-06-09 19:55:19 +0300

    part 1

--- virtual-memory-manager-project/virtmem.c
+++ virtual-memory-manager-project/virtmem.c
@@ -12,18 +12,19 @@
 
 #define TLB_SIZE 16
 #define PAGES 1024
-#define PAGE_MASK /* TODO */
+#define PAGE_MASK 1023 /* TODO */
 
 #define PAGE_SIZE 1024
 #define OFFSET_BITS 10
-#define OFFSET_MASK /* TODO */
+#define OFFSET_MASK 1023 /* TODO */
 
-#define MEMORY_SIZE PAGES * PAGE_SIZE
+#define MEMORY_SIZE PAGES *PAGE_SIZE
 
 // Max number of characters per line of input file to read.
 #define BUFFER_SIZE 10
 
-struct tlbentry {
+struct tlbentry
+{
   unsigned char logical;
   unsigned char physical;
 };
@@ -49,83 +50,98 @@ int max(int a, int b)
 }
 
 /* Returns the physical address from TLB or -1 if not present. */
-int search_tlb(unsigned char logical_page) {
-    /* TODO */
+int search_tlb(unsigned char logical_page)
+{
+  /* TODO */
+  return pagetable[logical_page];
 }
 
 /* Adds the specified mapping to the TLB, replacing the oldest mapping (FIFO replacement). */
-void add_to_tlb(unsigned char logical, unsigned char physical) {
-    /* TODO */
+void add_to_tlb(unsigned char logical, unsigned char physical)
+{
+  /* TODO */
+  memcpy(main_memory + physical * PAGE_SIZE, backing + logical * PAGE_SIZE, PAGE_SIZE);
+  pagetable[logical] = physical;
 }
 
 int main(int argc, const char *argv[])
 {
-  if (argc != 3) {
+  if (argc != 3)
+  {
     fprintf(stderr, "Usage ./virtmem backingstore input\n");
     exit(1);
   }
-  
-  const char *backing_filename = argv[1]; 
+
+  const char *backing_filename = argv[1];
   int backing_fd = open(backing_filename, O_RDONLY);
-  backing = mmap(0, MEMORY_SIZE, PROT_READ, MAP_PRIVATE, backing_fd, 0); 
-  
+  backing = mmap(0, MEMORY_SIZE, PROT_READ, MAP_PRIVATE, backing_fd, 0);
+
   const char *input_filename = argv[2];
   FILE *input_fp = fopen(input_filename, "r");
-  
+
   // Fill page table entries with -1 for initially empty table.
   int i;
-  for (i = 0; i < PAGES; i++) {
+  for (i = 0; i < PAGES; i++)
+  {
     pagetable[i] = -1;
   }
-  
+
   // Character buffer for reading lines of input file.
   char buffer[BUFFER_SIZE];
-  
+
   // Data we need to keep track of to compute stats at end.
   int total_addresses = 0;
   int tlb_hits = 0;
   int page_faults = 0;
-  
+
   // Number of the next unallocated physical page in main memory
   unsigned char free_page = 0;
-  
-  while (fgets(buffer, BUFFER_SIZE, input_fp) != NULL) {
+
+  while (fgets(buffer, BUFFER_SIZE, input_fp) != NULL)
+  {
     total_addresses++;
     int logical_address = atoi(buffer);
 
     /* TODO 
     / Calculate the page offset and logical page number from logical_address */
-    int offset =
-    int logical_page =
+    int offset = logical_address & OFFSET_MASK;
+    int logical_page = (logical_address >> OFFSET_BITS) & PAGE_MASK;
     ///////
-    
+
     int physical_page = search_tlb(logical_page);
     // TLB hit
-    if (physical_page != -1) {
+    if (physical_page != -1)
+    {
       tlb_hits++;
       // TLB miss
-    } else {
+    }
+    else
+    {
       physical_page = pagetable[logical_page];
-      
+
       // Page fault
-      if (physical_page == -1) {
-          /* TODO */
+      if (physical_page == -1)
+      {
+        /* TODO */
+        page_faults++;
+        physical_page = free_page;
+        free_page++;
       }
 
       add_to_tlb(logical_page, physical_page);
     }
-    
+
     int physical_address = (physical_page << OFFSET_BITS) | offset;
     signed char value = main_memory[physical_page * PAGE_SIZE + offset];
-    
+
     printf("Virtual address: %d Physical address: %d Value: %d\n", logical_address, physical_address, value);
   }
-  
+
   printf("Number of Translated Addresses = %d\n", total_addresses);
   printf("Page Faults = %d\n", page_faults);
   printf("Page Fault Rate = %.3f\n", page_faults / (1. * total_addresses));
   printf("TLB Hits = %d\n", tlb_hits);
   printf("TLB Hit Rate = %.3f\n", tlb_hits / (1. * total_addresses));
-  
+
   return 0;
 }
